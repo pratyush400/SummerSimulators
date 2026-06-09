117d3c8d50fa1bfd5f89f817e8c175bc5d7b6aa5
James Cunningham <jamespconyt@gmail.com>
2026-06-09 14:59:59 -0700

    established some psudocode to try and get a functional magnetic spinner

--- phase_and_frequency_encoding_sim.py
+++ phase_and_frequency_encoding_sim.py
@@ -30,18 +30,102 @@ control_panel = canvas(
     resizable = False
     )
     
-#----------------------------Create Objects----------------------------#
+#----------------------------Create Objects for animation pannel----------------------------#
+
+#create an empty list to track all the arrow assets
+
+#create a class that contains all infromation needed for rotating the "magnetic pointers" at different speeds
+class magnetic_pointer();
+
+    def __init__():
+        self.posistion = #take input for posistion
+
+        #set up the circle and arrow for the spinner/posistion
+        self.components = [
+            sphere(
+                canvas = animation_scene,
+                pos = vector(0,0,0),
+                radius = 1,
+                color = color.red,
+                visible = False
+            ),
+            arrow(
+                canvas = animation_scene,
+                pos = vector(0, 0, 0),
+                axis = vector(0, 1, 0),
+                shaftwidth =.2,
+                headwidth= .4,
+                headlength = .15,
+                color = color.blue,
+                round = True,
+                visible = True
+            )
+        ]
+        
+        self.magnetic_field = 0
+
+
+    def calculate_magnetic_field(self):
+
+        B = background_field + (x_pos * x_gradient) + (y_pos * x_gradient)
+        self.magnetic_field = B
+
+    #rotate for 1 ms
+    def rotate_self(self):
+
+        #calculate theta initial
+        unit_vector = target.x_pos / target.raidus
+        theta = invcos(unit_vector)
+
+        #calculate b_field
+        B = calculate_magnetic_field(self)
+        angular_velocity = B * rotation_coefficent
+
+        #find new theta and apply changes
+        theta = theta + angular_velocity
+        self.components[2].axis = vector(
+            cos(theta * raidus),
+            sin(theta * raidus),
+            0
+        )
 
 test_object_animation_space = sphere(
     canvas = animation_scene,
     pos = vector(0,0,0),
     radius = 1,
-    color = color.red
+    color = color.red,
+    visible = False
     )
+
+test_arrow = arrow(
+    canvas = animation_scene,
+    pos = vector(0, 0, 0),
+    axis = vector(0, 1, 0),
+    shaftwidth =.2,
+    headwidth= .4,
+    headlength = .15,
+    color = color.blue,
+    round = True,
+    visible = True
+)
+
+#----------------------------Create Objects for control pannel----------------------------#
+
     
 test_object_ctrl_panel = sphere(
     canvas = control_panel,
     pos = vector(0,0,0),
     radius = 1,
-    color = color.red
+    color = color.red,
+    visible = False
     )
+
+
+#----------------------------Create functions for animation pannel----------------------------#
+
+
+
+
+
+
+#----------------------------Create functions for control pannel----------------------------#
