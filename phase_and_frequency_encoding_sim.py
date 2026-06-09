b3d592097e6293e2dc645aacc4de6617f475598c
James Cunningham <jamespconyt@gmail.com>
2026-06-08 17:31:37 -0700

    added test objects

--- phase_and_frequency_encoding_sim.py
+++ phase_and_frequency_encoding_sim.py
@@ -1,11 +1,16 @@
-Web VPython 3.2
-from vpython import *
+Web VPython 3.2 
+#line above required for glowscript to run
 
+from vpython import *
+from random import choice
+
+
+#----------------------------Create Scenes----------------------------#
 animation_scene = canvas(
     width = 1024, 
     height = 480, 
     center = vector(0,0,0), 
-    background = color.black, 
+    background = vector(1,1,1), 
     resizable = False, 
     userzoom = False, 
     userspin = False
@@ -18,9 +23,25 @@ distant_light(direction = vector(-0.88, -0.22, -0.44), color = color.white)
 control_panel = canvas(
     width =1024, 
     height=100, 
-    center = vector(0,0,0), 
+    center = vector(0, 0, 0), 
     background = vec(0.622, 0.779, 0.847), 
     userspin = False, 
     userzoom = False, 
     resizable = False
+    )
+    
+#----------------------------Create Objects----------------------------#
+
+test_object_animation_space = sphere(
+    canvas = animation_scene,
+    pos = vector(0,0,0),
+    radius = 1,
+    color = color.red
+    )
+    
+test_object_ctrl_panel = sphere(
+    canvas = control_panel,
+    pos = vector(0,0,0),
+    radius = 1,
+    color = color.red
     )
